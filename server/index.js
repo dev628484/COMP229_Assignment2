c0061093fc289a668765905e4c20bfde897d6f92
Castilho <castilho@castleventures.ca>
2025-10-01 19:04:17 -0400

    Week 4 Code Completed: MONGO CRUD

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
 import express from 'express';
 import morgan from 'morgan';
+import mongoose from "mongoose";
+import dotenv from 'dotenv/config';
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI);
+const connection = mongoose.connection;
+connection.on('error', console.error.bind(console, "MongoDB connection error: "));
+connection.once('open', () => { console.log('Connected to MongoDB'); });
 
 import homeRoutes from './routes/homeRoutes.js';
+import projectRoutes from './routes/project.js';
 
 const app = express();
 
 app.use(morgan('dev'));
 
 //Routes
-app.use('/', homeRoutes)
+app.use('/', homeRoutes);
+app.use('/projects', projectRoutes);
 
 
 app.listen(3000);   
